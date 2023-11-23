bc60ddf6ea42086e75253bf273f1815a5fa02b1a
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2023-11-23 16:40:11 +0900

    helloボタンからapi/helloを呼び出せるようにした。

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 export default function MyHelloBtn(){
+  const handleClick = async () => {
+    try{
+      const response = await fetch('/api/hello');
+      const data = await response.json();
+      alert(JSON.stringify(data));
+    } catch (error) {
+      alert('エラーが発生しました: ' + error.message);
+    }
+  }
+
   return (
-    <button onClick={()=>alert('hello')}>Hello world!</button>
+    <button onClick={handleClick}>Hello world!</button>
   )
 }
